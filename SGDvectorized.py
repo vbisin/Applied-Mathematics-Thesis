Fixed W gradient error
Also various speed and readability updates

SGDvectorized.py
@@ -1,6 +1,4 @@
-
 ## Stochastic Gradient Descent Loop for Algorithm 
-
 
 import numpy as np
 from gradients import alphaGradient, WGradient
@@ -17,14 +15,14 @@ def multiSGDthres(x,y,alpha,W):
 
     #Learning Rates
     learningRateAlpha=.000005
-    learningRateW=.0000005     
+    learningRateW=.000005     
     
     # Divergence criterion 
     divergenceThreshold=10
     
     
     # Error criterion needed to exit Stochastic Gradient descent 
-    threshold=.05
+    threshold=.0005
     
 
 ## Graph Variables to return 
@@ -56,14 +54,14 @@ def multiSGDthres(x,y,alpha,W):
 ## Stochastic Gradient Descent loop, completes at least two epochs 
     # and exits if alpha and W grads' sum is less than the threshold
     
-    while (len(errorEpoch)<3 or (abs(alphaGradEpoch[len(alphaGradEpoch)-1])+abs(WGradEpoch[len(WGradEpoch)-1]))>threshold) and len(errorEpoch)<50:
-    #while (len(errorEpoch)<3 or (abs(alphaGradEpoch[len(alphaGradEpoch)-1]))>threshold) and len(errorEpoch)<20:
-    #while len(errorEpoch)<50:
+    while (len(errorEpoch)<3 or (abs(alphaGradEpoch[len(alphaGradEpoch)-1])+abs(WGradEpoch[len(WGradEpoch)-1]))>threshold) and len(errorEpoch)<30:
  
-       
+        # Matrix to be returned after each iteration of the SGD algorithm
+        # Returns the MSE error per sample, alpha gradient, and W gradient 
         returnMatrix=np.zeros((samples,3),dtype=object)
         returnMatrix=np.asarray([samplesSGDLoop(alphaChange,WChange,x[:,sample],y[:,sample],learningRateAlpha,learningRateW) for sample in samplesIterator])    
    
+    
         alpha=alphaChange
         W=WChange
         # For each epoch record average error of each sample
@@ -99,9 +97,10 @@ def samplesSGDLoop(alpha,W,sampleX,sampleY,learningRateAlpha,learningRateW):
     WGrad=WGradient(sampleX,sampleY,alpha,W)
     
     #Check alpha and W gradients using def. of derivative 
-    alphaGradCheck(sampleX,sampleY,alpha,W)
-    checker=WGradCheck(sampleX,sampleY,alpha,W)
-    print("The difference in W grad's summed up is: "+str(sum(sum(checker))))
+    
+    #alphaGradCheck(sampleX,sampleY,alpha,W)
+    #checker=WGradCheck(sampleX,sampleY,alpha,W)
+    #print("The difference in W grad's summed up is: "+str(sum(sum(checker))))
    
     ## Update alpha and W                 
     alpha=alpha-learningRateAlpha*alphaGrad

driver.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from rbf import rbfF
 import time
 import os
-from SGDvectorize import multiSGDthres
+from SGDvectorized import multiSGDthres
 from estimateSignal import estimateSignal
 start_time = time.time()
 import random
@@ -19,10 +19,10 @@ import random
 maxValue=100
 
 #Number of randomly generated signals to train over
-numberExamples=100
+numberExamples=10000
 
 #Length of each signal
-signalLength=20
+signalLength=50
 
 # Number of Gaussians in Gaussian RBF 
 alphaDim=11
@@ -34,24 +34,26 @@ noiseStdDev=5
 (x,y)=createSteps(numberExamples,signalLength,maxValue,noiseStdDev)
 
 ##W and alpha Initalization 
-W=np.zeros((signalLength,signalLength))   
-#for i in range(signalLength):
-#    W[i,:]=np.asarray([random.uniform(0, 1) for j in range(signalLength)]).astype(np.float)
-##
+W=np.zeros((signalLength,signalLength))
+
+# Random initialization   
 for i in range(signalLength):
-    if i==0:
-        W[i,i]=1
-    else:
-        np.fill_diagonal(W,1)
-        W[i,i-1]=-1
+    W[i,:]=np.asarray([random.uniform(0, 10) for j in range(signalLength)]).astype(np.float)
+    
+# Finite difference operator    
 
-          
-alpha=np.asarray([random.uniform(0, 1) for j in range(alphaDim)]).astype(np.float)        
+#for i in range(signalLength):
+#    if i==0:
+#        W[i,i]=1
+#    else:
+#        np.fill_diagonal(W,1)
+#        W[i,i-1]=-1
+
+# Random initialization          
+alpha=np.asarray([random.uniform(0, 10) for j in range(alphaDim)]).astype(np.float)        
+
+# Constant initialization 
 #alpha=np.ones(alphaDim)*.75
-
-#Approximate optimized values of alpha after SGD (used when only optimizing W)
-#alpha=np.array([3.86,8.08,12.05,9.9,3.47,.48,-2.54,-8.667,-10.63,-6.75,-2.4], dtype=np.float)              
-                  
 
 
 #Run the Stochastic Gradient Descent Algorithm
@@ -199,6 +201,16 @@ if len(WGradEpoch)>1:
     plt.xlabel('Iteration')
     plt.savefig('WGradsSum')
     plt.show
+
+
+plt.figure(11)
+plt.title("Influence Function")
+plt.plot(len(y[:,signal]),np.dot(rbfF(W,y[:,signal],len(alpha)),alpha),'b',label='Influence Function for Signal '+str(signal))
+plt.legend(bbox_to_anchor=(.52, .24), loc=0, borderaxespad=0.)
+plt.xlabel('Iteration')
+plt.savefig('influenceFunction')
+plt.show
+
 
 
 

gradientCheck.py
@@ -4,19 +4,19 @@ import numpy as np
 from estimateSignal import estimateSignal
 from gradients import alphaGradient, WGradient
 import copy
-#import random
 
 def alphaGradCheck(sampleX,sampleY,alpha,W):
     
     alphaDim=len(alpha)
     alphaIterator=np.arange(alphaDim)
-    # choose vector entry
     alphaSum=0
+    
     for i in alphaIterator:
         
+        # My calculated derivative estimate
         alphaGrad=alphaGradient(sampleX,sampleY,alpha,W)[i]
         
-        
+        # Estimate derivative using definition of derivative 
         epsilon=.0001
         alphaEpsPos=copy.deepcopy(alpha)
         alphaEpsPos[i]=alphaEpsPos[i]+epsilon
@@ -34,21 +34,29 @@ def alphaGradCheck(sampleX,sampleY,alpha,W):
             print("Alpha grad's "+str(i+1)+"th entry is incorrect. The difference between the two is: " + str(abs(alphaGrad-derivativeCheck)))
             bools=False
         
+        # Calculate alpha sum just to check that alpha gradient is non-constant 
         alphaSum=abs(alphaGrad)+alphaSum
             
     if bools==True:
-            print("Alpha gradient is correct (and sum over alphas is: " + str(alphaSum)+")")
-                   
+           # print("Alpha gradient is correct (and sum over alphas is: " + str(alphaSum)+")")
+            print("Alpha gradient is correct")
+     
              
 def WGradCheck(sampleX,sampleY,alpha,W):
         
     WIterator=np.arange(len(sampleX))
-    checkableW=np.zeros((len(sampleX),len(sampleX)))
-# choose matrix entry
-    checkableW=np.asarray([np.asarray([speedWGradCheck(i,j,sampleX,sampleY,alpha,W) for j in WIterator]).astype(np.float) for i in WIterator])
-    gradDifference=checkableW-WGradient(sampleX,sampleY,alpha,W)
+    correctGrads=np.zeros((len(sampleX),len(sampleX)))
+
+    # For each matrix entry, calculate estimated derivative using definition of gradient 
+    correctGrads=np.asarray([np.asarray([speedWGradCheck(i,j,sampleX,sampleY,alpha,W) for j in WIterator]).astype(np.float) for i in WIterator])
     
-    if (abs(gradDifference)<.01).all():
+    # My calculated derivatives 
+    myGrads=WGradient(sampleX,sampleY,alpha,W)
+    
+    # Calculate matrix of differences 
+    gradDifference=correctGrads-myGrads
+    
+    if (abs(gradDifference)<.5).all():
         print("W Gradient is correct")
         return gradDifference
     else:    
@@ -58,11 +66,13 @@ def WGradCheck(sampleX,sampleY,alpha,W):
 ## Sub function to speed up WGradCheck
 def speedWGradCheck(i,j,sampleX,sampleY,alpha,W):    
     
+    
+    # Calculate estimated derivative for each entry of W
     epsilon=.0001
     WEpsPos=copy.deepcopy(W)
     WEpsPos[i][j]=WEpsPos[i][j]+epsilon
     WEpsNeg=copy.deepcopy(W)
-    WEpsNeg[i][j]=WEpsPos[i][j]-epsilon
+    WEpsNeg[i][j]=WEpsNeg[i][j]-epsilon
     
     
     

gradients.py
@@ -63,20 +63,17 @@ def WGradient(sampleX,sampleY,alpha,W):
 
 ## Calculate LHS of product rule for dGammadW (i.e. -transpose(W)*dOmegadW)
     derivOmegadW=dOmegadW(sampleY,alpha,W)
-    LHSProductRule=np.dot(negTransW,derivOmegadW)
+    LHSProductRule=np.dot(derivOmegadW,negTransW)
 
-    
 ## Calculate RHS of product rule for dGammadW (i.e. d(-transpose(W))dW*omega) 
     #since d(-transpose(W))dW is the identity 4-tensor, then the above product will be a 3 tensor
     RHSProductRule=np.zeros((N,N,N))
     iterator=np.arange(N)
-    #LHSProductRule[iterator,iterator,iterator]=-omega[iterator]
-    RHSProductRule[iterator,iterator,iterator]=-omega[iterator]
+    RHSProductRule[iterator,iterator,:]=-omega[iterator]
 
 
 ## Calculate dFdW (i.e. dFdG*dGammadW)
     dFdG=dFdGamma(sampleX,sampleY,alpha,W)
-
-    dFdW=np.dot(dFdG,LHSProductRule+RHSProductRule)
+    dFdW=np.dot(np.transpose(LHSProductRule+RHSProductRule),dFdG)
 
     return dFdW