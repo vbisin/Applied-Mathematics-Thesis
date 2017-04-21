Correct W Gradient
Minor Readability  updates as well

createStep.py
@@ -1,7 +1,4 @@
 ## Creates random piece-wise constant functions 
-
-# Currently optimized for signals of length 50 
-
 import numpy as np
 import random 
 
@@ -18,21 +15,29 @@ def createSteps(samples,length,maxValue,noiseStd):
     iteratorSamples=np.arange(samples)
     iteratorLength=np.arange(length)    
     
+# Determine multiples of lenth of signal, to then define number of uniform "steps" of each step function
+    multiples=list()
+    iteratorMultiples=np.arange(length)+1
+    
+    for i in iteratorMultiples:
+        if length%i==0:
+            multiples.append(i)
+    
+    
 ## Function calling loop which defines each signal   
     
-    x=np.transpose(np.asarray([speedCreateSteps(length,maxValue) for i in iteratorSamples]).astype(np.float))
+    x=np.transpose(np.asarray([speedCreateSteps(length,maxValue,multiples) for i in iteratorSamples]).astype(np.float))
        
     
    ## For each point in the original x signal, we add random Gaussian noise (0,noiseStd) to define y
     y[iteratorLength,:]= np.transpose([np.asarray([x[i,j]+np.random.normal(0,noiseStd) for i in iteratorLength]).astype(np.float) for j in iteratorSamples])
 
-    
     return (x,y)   
 
-def speedCreateSteps(length,maxValue):
+def speedCreateSteps(length,maxValue,multiples):
 
- # For each signal randomly decide how many "steps" or changes in values there will be 
-    numberSteps=random.sample(set([1, 2, 4, 5,10,20,25]), 1)[0]
+ # For each signal randomly pick one of the above defined multiples, this will be the number of "steps" or changes in values there will be 
+    numberSteps=random.sample(multiples,1)[0]
     
     # Depending on the number of steps, we define a uniform "step" length for each signal
     intervalLength=length/numberSteps

dOmegadW.py
@@ -2,56 +2,48 @@
 # Where omega is defined as the RBF function
 
 import numpy as np
-
+from rbf import GRBFCenters
 def dOmegadW(sampleY,alpha,W):
     
     #Recover length of signal  
     N=len(sampleY)
-    
     #Variables needed for derivative calculation
-    iterator=np.arange(N)
+    iteratorN=np.arange(N)
     derivOmegadW=np.zeros((N,N,N))    
+   
+    
+## Define centers of Gaussian RBF, so that centers of Gaussians match interval 
+# of ranges of the signals  
+
+    # Compute step size and iterator for RBF 
+    (stepSize,iteratorMu)=GRBFCenters(np.dot(W,sampleY),len(alpha))
+
+
+    # Set sigma (standard deviation of each Gaussian) as the interval between each Gaussian center
+    sigma=float(stepSize)          
+    
     
 ##Calculate dOmegadW    
     for i in range(N): 
-        lvector=np.array([dOmegadWEntry(sampleY,l,W[i,:],alpha) for l in iterator]).astype(np.float)    
+        lvector=np.array([dOmegadWEntry(sampleY,l,W[i,:],alpha,sigma,iteratorMu) for l in iteratorN]).astype(np.float)    
         derivOmegadW[i,i,:]=lvector
             
     return derivOmegadW
 
 
-def dOmegadWEntry(sampleY,l,Wvector,alpha):
+def dOmegadWEntry(sampleY,l,Wvector,alpha,sigma,iteratorMu):
 
 ## Set up parameters    
     alphaDim=len(alpha)
     signal=np.dot(Wvector,sampleY)
     
-    # Set sigma (standard deviation of each Gaussian) as the interval between each Gaussian center
-    sigma=30.
-
-
-## Define centers of Gaussian RBF, so that centers of Gaussians match interval 
-# of ranges of the signals  
-    iteratorMu=np.zeros(alphaDim)
-    iteratorMakeMu=np.arange(alphaDim/2)    
-
-    # First define iterator array with values in the negative image range of the signal 
-    iteratorMakeMuRev=iteratorMakeMu[::-1]*(-30)-30
-    iteratorMu[iteratorMakeMu]=iteratorMakeMuRev[iteratorMakeMu]
     
-    # Define iterator array with values in the positive image range of the signal 
-    iteratorMu[iteratorMakeMu+alphaDim/2+1]=iteratorMakeMu*30+30
-    
-
     matrixEntry=np.zeros(alphaDim)
     iteratorAlpha=np.arange(alphaDim)        
 
-## Compute respective entry (i.e. the i,i,l) of dOmegadW 
+## Compute respective entry (i.e. the [i,i,l] entry) of dOmegadW 
     matrixEntry[iteratorAlpha]=((-sampleY[l]*alpha[iteratorAlpha]*(signal-iteratorMu[iteratorAlpha]))/(sigma**2))*(np.exp((-(signal-iteratorMu[iteratorAlpha])**2.)/(2.*sigma**2.)))
     matrixEntry=sum(matrixEntry)
     
    
     return matrixEntry
-
-
-

driver.py
@@ -7,11 +7,12 @@ import matplotlib.pyplot as plt
 from rbf import rbfF
 import time
 import os
-from SGDvectorized import multiSGDthres
+from sgd import multiSGDthres
 from estimateSignal import estimateSignal
 start_time = time.time()
 import random
 
+HPC=False
 
 ## Define starting parameters for algorithm 
 
@@ -19,7 +20,7 @@ import random
 maxValue=100
 
 #Number of randomly generated signals to train over
-numberExamples=10000
+numberExamples=1000
 
 #Length of each signal
 signalLength=50
@@ -30,37 +31,59 @@ alphaDim=11
 #Standard deviation of added noise 
 noiseStdDev=5
 
+# Step sizes/learning rates for Stochastic Gradient Descent
+if noiseStdDev<25:
+    learningRateAlpha=.0000005
+    learningRateW=.0000005    
+else: 
+    learningRateAlpha=.000005
+    learningRateW=.000005    
+    
+
 #Retrive randomly generated piecewise constant signals (x is original, y is noisy)
 (x,y)=createSteps(numberExamples,signalLength,maxValue,noiseStdDev)
 
 ##W and alpha Initalization 
 W=np.zeros((signalLength,signalLength))
 
-# Random initialization   
-for i in range(signalLength):
-    W[i,:]=np.asarray([random.uniform(0, 10) for j in range(signalLength)]).astype(np.float)
-    
-# Finite difference operator    
-
+# Random initialization 
+#print("W randomly initialized")  
 #for i in range(signalLength):
-#    if i==0:
-#        W[i,i]=1
-#    else:
-#        np.fill_diagonal(W,1)
-#        W[i,i-1]=-1
+#    W[i,:]=np.asarray([random.uniform(0, 10) for j in range(signalLength)]).astype(np.float)
+#    
+# Finite difference operator    
+print("W well initialized")
+for i in range(signalLength):
+    if i==0:
+        W[i,i]=1
+    else:
+        np.fill_diagonal(W,1)
+        W[i,i-1]=-1
 
-# Random initialization          
-alpha=np.asarray([random.uniform(0, 10) for j in range(alphaDim)]).astype(np.float)        
+# Random initialization 
+#print("alpha randomly initialized")         
+#alpha=np.asarray([random.uniform(0, 10) for j in range(alphaDim)]).astype(np.float)        
 
 # Constant initialization 
-#alpha=np.ones(alphaDim)*.75
-
-
+print("alpha well initialized")
+alpha=np.ones(alphaDim)*.75
+             
+# Function initializing derivative of W transpose w.r.t W, so only need to compute once
+negWTransDeriv=np.zeros((signalLength,signalLength,signalLength,signalLength))
+for k in range(signalLength):
+   for l in range(signalLength):
+       negWTransDeriv[l,k,k,l]=-1 
+            
+      
 #Run the Stochastic Gradient Descent Algorithm
-(alpha, W, errorEpoch,alphaHistory,WHistory,learningRates,alphaGradEpoch,WGradEpoch)=multiSGDthres(x,y,alpha,W)
+(alpha, W, errorEpoch,alphaHistory,WHistory,learningRates,alphaGradEpoch,WGradEpoch)=multiSGDthres(x,y,alpha,W,learningRateAlpha,learningRateW,negWTransDeriv)
 
-
-
+# Used when running on HPC
+#os.chdir('/home/vb704/Learning-Optimal-Filter-and-Penalty-Function')
+      
+#Exemplary signal to graph 
+signal=0
+    
 
 ## Graphing parameters 
 epochsArray=np.arange(len(errorEpoch))
@@ -69,10 +92,7 @@ iteratorN=np.arange(signalLength)
 iteratorSamples=np.arange(numberExamples)
 EPOCH=(len(epochsArray)-1)*numberExamples+1
 iteratorEpochs=np.arange(EPOCH)
-      
-#Exemplary signal to graph 
-signal=0
-    
+
 #Calculate final estimated signal for each signal (i.e. after the algorithm has converged)
 finalEstimates=np.zeros((signalLength,numberExamples))
 finalEstimates[:,iteratorSamples]=np.transpose(np.asarray([estimateSignal(W,y[:,j],alpha)-y[:,j] for j in iteratorSamples]).astype(np.float))
@@ -97,17 +117,23 @@ plt.plot(iteratorN,x[:,signal],'b',label='Original signal: '+str(signal+1))
 plt.plot(iteratorN,y[:,signal],'r',label='Noisy signal (sigma='+str(noiseStdDev)+'): '+str(signal+1))
 plt.legend(bbox_to_anchor=(.55, .85), loc=0, borderaxespad=0.)
 plt.title("Sample of Original and Noisy Signals ("+str(signal+1)+" of "+ str(numberExamples)+")")
-plt.savefig('signals')
-plt.show
+if HPC:
+  plt.savefig("/home/vb704/Learning-Optimal-Filter-and-Penalty-Function/signals")  
+else:    
+    plt.savefig('signals')
+    plt.show
 
 # Calculates MSE over each epoch
 plt.figure(2)
 plt.plot(epochsArray,errorEpoch,'r')
-plt.title("Mean MSE per Epoch")
+plt.title("MSE per Epoch")
 plt.xlabel('Epochs')
 plt.ylabel('Mean Squared Error')
-plt.savefig('MSE')
-plt.show
+if HPC:
+  plt.savefig("/home/vb704/Learning-Optimal-Filter-and-Penalty-Function/MSE")  
+else:  
+    plt.savefig('MSE')
+    plt.show
 
 # Final estimate of alphas
 plt.figure(3)
@@ -115,16 +141,23 @@ plt.plot(iteratorAlpha+1,alpha,'b',label='Alphas')
 plt.title("RBF Parameter Values: Alpha")
 plt.xlabel('Alpha Number')
 plt.ylabel('Alpha Value')
-plt.savefig('alphas')
-plt.show
+if HPC:
+  plt.savefig("/home/vb704/Learning-Optimal-Filter-and-Penalty-Function/alphas")  
+else:  
+    plt.savefig('alphas')
+    plt.show
+
 
 
 # Heat map of learned kernel (W)
 plt.figure(4)
 plt.imshow(W, cmap='hot', interpolation='nearest')
 plt.title("Heat Map of Kernel Matrix")
-plt.savefig('kernelHeat')
-plt.show()
+if HPC:
+  plt.savefig("/home/vb704/Learning-Optimal-Filter-and-Penalty-Function/kernelHeat")  
+else:  
+    plt.savefig('kernelHeat')
+    plt.show()
 
 
 #Calculate sum of function evaluation estimates w.r.t. epochs
@@ -143,8 +176,11 @@ plt.title("Sum of Estimates")
 plt.legend(bbox_to_anchor=(.24, .89), loc=0, borderaxespad=0.)
 plt.ylabel('Estimate Sum')
 plt.xlabel('Iteration')
-plt.savefig('sumEstimate')
-plt.show
+if HPC:
+  plt.savefig("/home/vb704/Learning-Optimal-Filter-and-Penalty-Function/sumEstimate")  
+else:  
+    plt.savefig('sumEstimate')
+    plt.show()
 
 
 WSum[iteratorEpochs]=np.sum(np.sum(np.abs(WHistory[iteratorEpochs]),axis=1),axis=1)
@@ -155,20 +191,25 @@ plt.title("Sum of Absolute values of W")
 plt.legend(bbox_to_anchor=(.24, .89), loc=0, borderaxespad=0.)
 plt.ylabel('Sum')
 plt.xlabel('Iteration')
-plt.savefig('Wsum')
-plt.show
+if HPC:
+  plt.savefig("/home/vb704/Learning-Optimal-Filter-and-Penalty-Function/Wsum")  
+else:  
+    plt.savefig('Wsum')
+    plt.show()
 
 
 # Absolute sums of alpha w.r.t. epochs
 plt.figure(7)
 plt.plot(iteratorEpochs,alphaSum,'b',label='Sum of Absolute Value of alpha')
-plt.title("Sum of alpha")
+plt.title("Sum of Absolute Value of alpha")
 plt.legend(bbox_to_anchor=(.24, .89), loc=0, borderaxespad=0.)
 plt.ylabel('Sum')
 plt.xlabel('Iteration')
-plt.savefig('alphaSum')
-plt.show
-
+if HPC:
+  plt.savefig("/home/vb704/Learning-Optimal-Filter-and-Penalty-Function/alphaSum")  
+else:  
+    plt.savefig('alphaSum')
+    plt.show()
 
 
 # Original, noisy, and predicted signals 
@@ -179,8 +220,11 @@ plt.plot(iteratorN,y[:,signal],'r',label='Noisy Signal (sigma='+str(noiseStdDev)
 plt.plot(iteratorN,y[:,signal]+finalEstimates[:,signal],'k',label='Predicted Signal')
 plt.legend(bbox_to_anchor=(.52, .24), loc=0, borderaxespad=0.)
 plt.xlabel('Iteration')
-plt.savefig('estimates')
-plt.show
+if HPC:
+  plt.savefig("/home/vb704/Learning-Optimal-Filter-and-Penalty-Function/estimates")  
+else:  
+    plt.savefig('estimates')
+    plt.show
 
 gradIterator=np.arange(len(alphaGradEpoch))
 
@@ -189,8 +233,11 @@ plt.title("Average sum of Alpha Gradient per Epoch")
 plt.plot(gradIterator,alphaGradEpoch,'b',label='Average sum of Alpha Gradient per Epoch')
 plt.legend(bbox_to_anchor=(.52, .24), loc=0, borderaxespad=0.)
 plt.xlabel('Iteration')
-plt.savefig('AlphaGradsSum')
-plt.show
+if HPC:
+  plt.savefig("/home/vb704/Learning-Optimal-Filter-and-Penalty-Function/alphaGradsSum")  
+else:  
+    plt.savefig('AlphaGradsSum')
+    plt.show
 
 
 if len(WGradEpoch)>1:
@@ -199,17 +246,24 @@ if len(WGradEpoch)>1:
     plt.plot(gradIterator,WGradEpoch,'b',label='Average sum of W Gradient per Epoch')
     plt.legend(bbox_to_anchor=(.52, .24), loc=0, borderaxespad=0.)
     plt.xlabel('Iteration')
+if HPC:
+  plt.savefig("/home/vb704/Learning-Optimal-Filter-and-Penalty-Function/WGradsSum")  
+else:  
     plt.savefig('WGradsSum')
     plt.show
 
-
+influence=np.dot(rbfF(W,y[:,signal],len(alpha)),alpha)
 plt.figure(11)
 plt.title("Influence Function")
-plt.plot(len(y[:,signal]),np.dot(rbfF(W,y[:,signal],len(alpha)),alpha),'b',label='Influence Function for Signal '+str(signal))
+plt.plot(np.arange(len(y[:,signal])),influence,'b',label='Influence Function for Signal '+str(signal))
+plt.plot(np.arange(len(y[:,signal])),np.dot(W,y[:,signal]),'r',label='Original Convolved Image for Signal '+str(signal))
 plt.legend(bbox_to_anchor=(.52, .24), loc=0, borderaxespad=0.)
 plt.xlabel('Iteration')
-plt.savefig('influenceFunction')
-plt.show
+if HPC:
+  plt.savefig("/home/vb704/Learning-Optimal-Filter-and-Penalty-Function/influenceFunction")  
+else:  
+    plt.savefig('influenceFunction')
+    plt.show
 
 
 

gradients.py
@@ -1,10 +1,10 @@
 ## Solves for gradients of loss function - w.r.t alpha and W
 
-
 import numpy as np
 from rbf import rbfF
 from dOmegadW import dOmegadW
 from estimateSignal import estimateSignal
+import scipy 
 
 
 ## Computes the dFdGamma derivative used in both the alpha and W gradients
@@ -35,45 +35,35 @@ def alphaGradient(sampleX,sampleY,alpha,W):
 ## Calculate dGammaddAlpha 
     dGdA=np.asarray([np.asarray([np.dot(negWTrans[i,:],(rbfWY[:,j])) for j in iteratorAlpha]).astype(np.float) for i in iteratorN])
     
-    
 ## Calculate dot product from chain rule (dFdGamma=dFdGamma*dGammadAlpha) 
-    dFdG=dFdGamma(sampleX,sampleY,alpha,W)
-    
+    dFdG=dFdGamma(sampleX,sampleY,alpha,W)  
     dFdA=np.dot(dFdG,dGdA) 
     
     return dFdA
 
 
-
-
-
-
 ## Compute the derivative w.r.t. W
-def WGradient(sampleX,sampleY,alpha,W):
+def WGradient(sampleX,sampleY,alpha,W,negWTransDeriv):
     
     # Recover respective lengths
-    N=len(sampleX)
     alphaDim=len(alpha)
     negTransW=-np.transpose(W)
     
+## Calculate LHS of product rule for dGammadW (i.e. -transpose(W)*dOmegadW)
+    derivOmegadW=dOmegadW(sampleY,alpha,W)
+    LHSProductRule=scipy.tensordot(negTransW,derivOmegadW, axes=[1,0])
+ 
+## Calculate RHS of product rule for dGammadW (i.e. d(-transpose(W))dW*omega) 
+    #since d(-transpose(W))dW is a 4-tensor, then the above product will be a 3 tensor
     
     # Define omega as the Gaussian RBF Function
     rbfWY=rbfF(W,sampleY,alphaDim)
     omega=np.dot(rbfWY,alpha)
-
-## Calculate LHS of product rule for dGammadW (i.e. -transpose(W)*dOmegadW)
-    derivOmegadW=dOmegadW(sampleY,alpha,W)
-    LHSProductRule=np.dot(derivOmegadW,negTransW)
-
-## Calculate RHS of product rule for dGammadW (i.e. d(-transpose(W))dW*omega) 
-    #since d(-transpose(W))dW is the identity 4-tensor, then the above product will be a 3 tensor
-    RHSProductRule=np.zeros((N,N,N))
-    iterator=np.arange(N)
-    RHSProductRule[iterator,iterator,:]=-omega[iterator]
-
+ 
+    RHSProductRule=scipy.tensordot(negWTransDeriv,omega, axes=[1,0])
 
 ## Calculate dFdW (i.e. dFdG*dGammadW)
     dFdG=dFdGamma(sampleX,sampleY,alpha,W)
-    dFdW=np.dot(np.transpose(LHSProductRule+RHSProductRule),dFdG)
-
+    dFdW=scipy.tensordot(dFdG,LHSProductRule+RHSProductRule, axes=[0,0])
+        
     return dFdW

rbf.py
@@ -7,23 +7,49 @@ def rbfF(W,sampleY,alphaDim):
     signal=np.dot(W,sampleY)
     N=len(signal)
     iteratorN=np.arange(N)
-    
-    
     gaussiansMatrix=np.zeros((N,alphaDim))
     
-    # Set sigma to be the length between Gaussian centers
-    sigma=30.
-    
-    # First define iterator array with values in the negative image range of the signal 
-    iteratorMu=np.zeros(alphaDim)
-    iteratorMakeMu=np.arange(alphaDim/2)
-    iteratorMakeMuRev=iteratorMakeMu[::-1]*(-30)-30
+    # Compute step size and iterator for RBF 
+    (stepSize,iteratorMu)=GRBFCenters(np.dot(W,sampleY),alphaDim)
 
-    # Define iterator array with values in the positive image range of the signal 
-    iteratorMu[iteratorMakeMu]=iteratorMakeMuRev[iteratorMakeMu]
-    iteratorMu[iteratorMakeMu+alphaDim/2+1]=iteratorMakeMu*30+30
     
+    # Set sigma to be the length between Gaussian centers
+    sigma=float(stepSize)
+              
 ## Calculate matrix of RBF Gaussians for W*y       
     gaussiansMatrix=np.asarray([np.asarray([np.exp((-(signal[i]-center)**2.)/(2.*sigma**2.)) for center in iteratorMu]).astype(np.float64) for i in iteratorN])
             
     return gaussiansMatrix
+
+
+def GRBFCenters(signal,alphaDim):
+    # Define min and max of the input signal
+    minSig=min(signal)
+    maxSig=max(signal)
+    
+    # Calculate total length of the interval between the max and min  
+    if maxSig<0 and minSig>0:
+        print("code needs to be fixed here")
+    interval=abs(int(maxSig)-int(minSig))
+    
+    # Keep increasing interval until it is divisible by the dimension of alpha
+    freedom=False
+    while freedom==False:
+        if interval%(alphaDim-1)!=0:
+            interval=interval+1
+        else:
+            break
+    
+    # Calculate step size so that given current dimension of alpha, we can span the entire interval
+    stepSize=interval/float(alphaDim-1)
+    
+    # Make array with corresponding step size so that spans entire interval
+    iteratorMakeMu=np.arange(alphaDim)*stepSize
+    
+    # Define an array using step size so that it covers the entire interval                         
+    iteratorAlpha=np.arange(alphaDim)
+    iteratorMu=np.zeros(alphaDim)
+    minSig=int(min(signal))
+    iteratorMu[iteratorAlpha]=iteratorMakeMu[iteratorAlpha]+minSig   
+        
+    return (stepSize, iteratorMu)